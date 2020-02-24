9a74c22d0238690a470d93927c12e369b8af44e2
Belami Nizeyimana <nizebelami@hotmail.com>
2020-02-25 00:31:46 +0100

    Boutton signup

--- app/src/main/java/com/example/sportmatcher/ui/MainActivity.kt
+++ app/src/main/java/com/example/sportmatcher/ui/MainActivity.kt
@@ -1,13 +1,24 @@
 package com.example.sportmatcher.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.sportmatcher.R
+import kotlinx.android.synthetic.*
+import kotlinx.android.synthetic.main.login_layout.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.login_layout)
+
+        val signup: Button = findViewById(R.id.signup)
+        signup.setOnClickListener(){
+            val intent = Intent(this, Signup::class.java)
+            startActivity(intent)
+        }
+
     }
 }
