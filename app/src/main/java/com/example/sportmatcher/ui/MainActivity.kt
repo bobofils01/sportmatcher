2776c1471a0d924463db947424bf8143496c2912
romanskz <skubiszewski.roman@gmail.com>
2020-02-29 12:24:09 +0100

    Test d'ajout map dans login (fonctionne pas)

--- app/src/main/java/com/example/sportmatcher/ui/MainActivity.kt
+++ app/src/main/java/com/example/sportmatcher/ui/MainActivity.kt
@@ -7,14 +7,26 @@ import android.util.Log
 import android.widget.Button
 import androidx.appcompat.app.AlertDialog
 import com.example.sportmatcher.R
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import kotlinx.android.synthetic.*
 import kotlinx.android.synthetic.main.login_layout.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var mMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_layout)
+
+        val mapFragment = childFragmentManager
+            .findFragmentById(R.id.fragment)
+                as SupportMapFragment
+        mapFragment.getMapAsync(this)
 
         val login: Button = findViewById(R.id.btn_login)
         login.setOnClickListener(){
@@ -36,5 +48,13 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 }
