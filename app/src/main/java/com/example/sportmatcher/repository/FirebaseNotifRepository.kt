572d5e72964611ad080d3c76f22e5b0cb2572fb5
Jean Bosco Rwibutso <jeanbosco@tricount.com>
2020-03-01 17:13:47 +0100

    notification use cases

---
+++ app/src/main/java/com/example/sportmatcher/repository/FirebaseNotifRepository.kt
@@ -0,0 +1,57 @@
+package com.example.sportmatcher.repository
+
+import android.content.ContentValues.TAG
+import android.util.Log
+import com.example.sportmatcher.model.User
+import com.google.firebase.database.*
+import io.reactivex.Observable
+import java.util.ArrayList
+
+class FirebaseNotifRepository : INotificationsRepository {
+
+    companion object {
+        private const val PATH = "notifiableDevices"
+    }
+
+    private val userTableRef by lazy {
+        FirebaseDatabase.getInstance().getReference("/${FirebaseNotifRepository.PATH}")
+    }
+
+    override fun getNotifiableDevices() : Observable<String> {
+
+        return Observable.create<String>{
+            emitter ->
+            userTableRef.addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    dataSnapshot.children.mapNotNull{ it ->
+                        it.getValue(String::class.java)?.let {
+                           emitter.onNext(it)
+                        }
+                    }
+
+                }
+                override fun onCancelled(databaseError: DatabaseError) {
+                    emitter.onError(
+                        databaseError.toException()
+                            ?: IllegalStateException("Firebase error received")
+                    )
+                }
+            })
+        }
+
+    }
+
+    override fun addNotifiableDevice(token: String?) {
+        if(token != null){
+            //TODO add in the database sorted by sportCategory
+            val key  = userTableRef.push().key
+
+            if (key == null) {
+                Log.w(TAG, "Couldn't get push key for notifiableDevices")
+                return
+            }
+            userTableRef.child(key).setValue(token)
+
+        }
+    }
+}
