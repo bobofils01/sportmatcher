8715d654deec79162709fe201759cdb7f9f16b8b
Belami Nizeyimana <nizebelami@hotmail.com>
2020-03-08 16:59:55 +0100

    Design

--- app/src/main/java/com/example/sportmatcher/ui/LoginActivity.kt
+++ app/src/main/java/com/example/sportmatcher/ui/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.example.sportmatcher.ui
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
--- app/src/main/java/com/example/sportmatcher/ui/WelcomeActivity.kt
+++ app/src/main/java/com/example/sportmatcher/ui/WelcomeActivity.kt
@@ -1,11 +1,44 @@
 package com.example.sportmatcher.ui
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.sportmatcher.R
+import com.example.sportmatcher.model.authentication.AuthenticatedState
+import com.example.sportmatcher.model.authentication.AuthenticationInProgress
+import com.example.sportmatcher.ui.authentication.LoginFragment
+import com.example.sportmatcher.ui.authentication.LoginViewState
+import kotlin.math.sign
+import com.example.sportmatcher.ui.authentication.SignUpFragment
+import com.example.sportmatcher.viewModels.authentication.LoginViewModel
+import com.google.android.gms.common.api.internal.LifecycleCallback.getFragment
+import androidx.lifecycle.Observer
 
 class WelcomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.welcome_layout)
+
+        val login : Button = findViewById(R.id.btn_login)
+        val signup : Button = findViewById(R.id.signup_btn)
+
+
+        login.setOnClickListener({
+            startActivity(Intent(this, LoginActivity::class.java))
+        })
+
+        signup.setOnClickListener({
+            startActivity(Intent(this, LoginActivity::class.java))
+        })
     }
 }
