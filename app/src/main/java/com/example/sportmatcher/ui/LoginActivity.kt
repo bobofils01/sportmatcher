b093325a165d5112463dfd98c2ee6f1649fb4a56
Belami Nizeyimana <nizebelami@hotmail.com>
2020-03-09 13:14:30 +0100

    Design+

--- app/src/main/java/com/example/sportmatcher/ui/LoginActivity.kt
+++ app/src/main/java/com/example/sportmatcher/ui/LoginActivity.kt
@@ -4,6 +4,10 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
@@ -37,12 +41,29 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        /*val email: EditText = findViewById(R.id.email)
+        val password: EditText = findViewById(R.id.password)
+        val login: Button = findViewById(R.id.btn_login)*/
+
+        /*password.addTextChangedListener(object: TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                login.isEnabled = email.text.toString().equals("") == true && password.text.toString().equals("") == true
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+        })*/
+
         intent.extras?.getString("key")
         initLiveDatas()
     }
 
     private fun initLiveDatas(){
         viewModel.getAuthenticationStateLiveDate().observe(this, Observer {
+
             it?.let { state ->
                 when (state) {
                     is AuthenticatedState -> {
