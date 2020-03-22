dee14143677f3f02843b9ac0a4c5d9cf5e960f39
Belami Nizeyimana <nizebelami@hotmail.com>
2020-03-22 19:47:00 +0100

    Progress Bar while logging in

---
+++ app/src/main/java/com/example/sportmatcher/ui/preferences/PreferencesActivity.kt
@@ -0,0 +1,11 @@
+package com.example.sportmatcher.ui.preferences
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PreferencesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
