9b25d4b5dac7708e8cf699a31b0cb79bff87333e
Belami Nizeyimana <nizebelami@hotmail.com>
2020-04-01 18:04:31 +0200

    Sport List (update)

--- app/src/main/java/com/example/sportmatcher/ui/SportChoiceFragment.kt
+++ app/src/main/java/com/example/sportmatcher/ui/SportChoiceFragment.kt
@@ -6,14 +6,16 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
-import android.widget.Button
-import android.widget.Toast
-
-import com.example.sportmatcher.R
+import android.widget.LinearLayout
 import com.example.sportmatcher.di.ServiceProvider
-import com.example.sportmatcher.ui.preferences.PreferencesActivity
 import com.example.sportmatcher.ui.sports.SportHomePageActivity
 import kotlinx.android.synthetic.main.sport_choice_layout.*
+import android.graphics.Typeface
+import android.util.TypedValue
+import android.widget.ImageView
+import android.widget.TextView
+
+
 
 /**
  * A simple [Fragment] subclass.
@@ -25,12 +27,17 @@ class SportChoiceFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.sport_choice_layout, container, false)
+        return inflater.inflate(com.example.sportmatcher.R.layout.sport_choice_layout, container, false)
     }
 
     @SuppressLint("CheckResult")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val sportsLogo = ArrayList<Int>()
+        sportsLogo.add(com.example.sportmatcher.R.drawable.basketball)
+        sportsLogo.add(com.example.sportmatcher.R.drawable.soccer)
+        sportsLogo.add(com.example.sportmatcher.R.drawable.tennis)
 
         /*setSupportActionBar(findViewById(R.id.toolbar))
         supportActionBar?.setDisplayHomeAsUpEnabled(false)*/
@@ -38,9 +45,51 @@ class SportChoiceFragment : Fragment() {
         ServiceProvider.getAllSportsUseCase.execute().subscribe{ sports ->
             sports_choice_list.removeAllViews()
 
+            var sportAt: Int = 0
             for(sport in sports){
 
-                val btn_sport = Button(activity)
+                val choice = LinearLayout(activity)
+                val params = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+                params.setMargins(25, 50, 0, 50)
+                choice.layoutParams = params
+                choice.orientation = LinearLayout.HORIZONTAL
+                choice.isClickable = true
+
+                val logo = ImageView(activity)
+                logo.setImageResource(sportsLogo[sportAt])
+                val logoSize = LinearLayout.LayoutParams(100, 100)
+                logoSize.setMargins(0, 0, 75, 0)
+                logo.layoutParams = logoSize
+                choice.addView(logo)
+
+                val sportName = TextView(activity)
+                sportName.text = sport.toUpperCase()
+                sportName.setTextSize(TypedValue.COMPLEX_UNIT_SP,25F)
+                sportName.setTypeface(null, Typeface.BOLD)
+                choice.addView(sportName)
+
+                choice.setOnClickListener{
+                        it -> onSportClick(sportName.text.toString())
+                }
+
+                sports_choice_list.addView(choice)
+
+                val split = LinearLayout(activity)
+                val splitParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+                splitParams.setMargins(0, 0, 0, 0)
+                choice.layoutParams = params
+                choice.orientation = LinearLayout.HORIZONTAL
+
+                val line = View(activity)
+                line.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 5)
+                line.setBackgroundResource(com.example.sportmatcher.R.color.colorGrey)
+                split.addView(line)
+
+                sports_choice_list.addView(split)
+
+                sportAt = sportAt.inc()
+
+                /*val btn_sport = Button(activity)
                 btn_sport.layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, dpToPx(82))
                 btn_sport.text = sport.toUpperCase()
                 //btn_sport.background = ResourcesCompat.getDrawable(resources, R.drawable.yours, null)
@@ -48,7 +97,7 @@ class SportChoiceFragment : Fragment() {
                         it -> onSportClick(it)
                 }
 
-                sports_choice_list.addView(btn_sport)
+                sports_choice_list.addView(btn_sport)*/
             }
         }
 
@@ -99,9 +148,7 @@ class SportChoiceFragment : Fragment() {
         }
     }*/
 
-    private fun onSportClick(v: View) {
-        val b = v as Button
-        val sportName = b.text.toString()
+    private fun onSportClick(sportName: String) {
         val intent = Intent(activity, SportHomePageActivity::class.java).apply {
             putExtra("SPORT_NAME", sportName)
         }
