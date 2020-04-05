6d429c45ce2b1a526449572b06f8aefae83803f7
Belami Nizeyimana <nizebelami@hotmail.com>
2020-04-05 18:51:52 +0200

    Log out (update)

--- app/src/main/java/com/example/sportmatcher/ui/SettingsFragment.kt
+++ app/src/main/java/com/example/sportmatcher/ui/SettingsFragment.kt
@@ -1,25 +1,33 @@
 package com.example.sportmatcher.ui
 
 
+import android.app.ProgressDialog
 import android.content.Intent
 import android.graphics.Typeface
 import android.os.Bundle
+import android.os.Handler
 import android.util.TypedValue
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
+import android.widget.ProgressBar
 import android.widget.TextView
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import com.example.sportmatcher.R
 import com.example.sportmatcher.viewModels.authentication.LogOutViewModel
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.login_layout.*
+import kotlinx.android.synthetic.main.progress_bar_layout.view.*
 import kotlinx.android.synthetic.main.settings_layout.*
+import kotlinx.android.synthetic.main.signup_layout.*
 
 /**
  * A simple [Fragment] subclass.
  */
+@Suppress("DEPRECATION")
 class SettingsFragment : Fragment() {
 
     private val viewModel: LogOutViewModel by lazy {
@@ -36,17 +44,26 @@ class SettingsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val progress = ProgressDialog(activity)
+        progress.setMessage("Logging out")
+        progress.setCancelable(false)
+        progress.isIndeterminate = true
+
         val logoSize = LinearLayout.LayoutParams(100, 100)
         logoSize.setMargins(0, 0, 75, 0)
         open_door.layoutParams = logoSize
 
-
         log_out_text.setTextSize(TypedValue.COMPLEX_UNIT_SP,25F)
 
         log_out.setOnClickListener{
-            viewModel.onLogoutClicked()
-            Toast.makeText(activity,"Logged out",Toast.LENGTH_LONG).show()
-            startActivity(Intent(activity, WelcomeActivity::class.java))
+            progress.show()
+
+            val handler = Handler()
+            handler.postDelayed({run{
+                viewModel.onLogoutClicked()
+
+                startActivity(Intent(activity, WelcomeActivity::class.java))
+            }}, 2000)
         }
     }
 
