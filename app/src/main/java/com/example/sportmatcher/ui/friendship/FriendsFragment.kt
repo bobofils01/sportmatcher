6f3928c696f37c67c111b64c19592ea15bda8b8f
romanskz <skubiszewski.roman@gmail.com>
2020-04-06 18:36:33 +0200

    Suppresion useless onAttach

--- app/src/main/java/com/example/sportmatcher/ui/friendship/FriendsFragment.kt
+++ app/src/main/java/com/example/sportmatcher/ui/friendship/FriendsFragment.kt
@@ -1,8 +1,6 @@
 package com.example.sportmatcher.ui.friendship
 
-import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,7 +12,6 @@ import com.example.sportmatcher.R
 import com.example.sportmatcher.adapters.FriendListAdapter
 import com.example.sportmatcher.adapters.UsersListAdapter
 import com.example.sportmatcher.di.ServiceProvider.getAuthenticatedUserUseCase
-import com.example.sportmatcher.model.User
 import com.example.sportmatcher.viewModels.friendship.FriendsViewModel
 import kotlinx.android.synthetic.main.friends_layout.*
 
@@ -33,17 +30,12 @@ class FriendsFragment: Fragment() {
         return inflater.inflate(R.layout.friends_layout, container, false)
     }
 
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         val from = getAuthenticatedUserUseCase.execute()
-        Log.d("Roman", "attach "+from?.toMap())
         if (from != null) {
             friendsViewModel.setUser(from!!)
         }
-    }
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
         val usersListView = all_users_list as ListView
 
         val friendsListView = friends_list as ListView
