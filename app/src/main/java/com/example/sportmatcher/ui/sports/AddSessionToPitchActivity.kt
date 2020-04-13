e9ff496a234a8814743db6069f0fa100652fd8e5
Belami Nizeyimana <nizebelami@hotmail.com>
2020-04-13 16:13:14 +0200

    Design

--- app/src/main/java/com/example/sportmatcher/ui/sports/AddSessionToPitchActivity.kt
+++ app/src/main/java/com/example/sportmatcher/ui/sports/AddSessionToPitchActivity.kt
@@ -1,5 +1,7 @@
 package com.example.sportmatcher.ui.sports
 
+import android.app.Activity
+import android.app.AlertDialog
 import android.app.DatePickerDialog
 import android.app.TimePickerDialog
 import android.content.Context
@@ -7,13 +9,19 @@ import android.content.Intent
 import android.graphics.Color
 import android.graphics.Paint
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.Toast
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.startActivity
+import androidx.core.view.size
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.sportmatcher.R
+import com.example.sportmatcher.R.*
 import com.example.sportmatcher.adapters.PlayersListAdapter
 import com.example.sportmatcher.di.ServiceProvider
 import com.example.sportmatcher.model.User
@@ -22,7 +30,9 @@ import com.example.sportmatcher.model.sport.Session
 import com.example.sportmatcher.ui.utils.UIUtils
 import com.example.sportmatcher.viewModels.sports.AddSessionViewModel
 import kotlinx.android.synthetic.main.add_session_layout.*
+import kotlinx.android.synthetic.main.session_info_layout.*
 import java.util.*
+
 
 class AddSessionToPitchActivity : AppCompatActivity() {
 
@@ -43,7 +53,8 @@ class AddSessionToPitchActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.add_session_layout)
+        setContentView(layout.add_session_layout)
+        supportActionBar?.setDisplayHomeAsUpEnabled(false)
         //val binding: AddSessionViewBinding = DataBindingUtil.setContentView(this, R.layout.add_session_layout)
         //binding = DataBindingUtil.inflate(layoutInflater, R.layout.add_session_layout, null, true)
         //binding.addSessionViewModel = addSessionViewModel
@@ -60,8 +71,43 @@ class AddSessionToPitchActivity : AppCompatActivity() {
         initPlayersListView()
         initDateTimePicker()
 
+        //Permet de débloquer le button Next
+        findViewById<EditText>(id.session_title).addTextChangedListener(infoTextWatcher)
+        findViewById<EditText>(id.session_date).addTextChangedListener(infoTextWatcher)
+        findViewById<EditText>(id.session_time).addTextChangedListener(infoTextWatcher)
+        findViewById<EditText>(id.session_description).addTextChangedListener(infoTextWatcher)
 
+        findViewById<Button>(id.next).setOnClickListener(){
+            hideKeyboard(this)
 
+            findViewById<LinearLayout>(id.session_info).visibility = View.GONE
+        }
+    }
+
+    //Retire le clavier de l'écran
+    fun hideKeyboard(activity: Activity) {
+        val imm = activity.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        var view = activity.currentFocus
+        if (view == null)
+            view = View(activity)
+
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    //Débloque le button Next
+    private val infoTextWatcher = object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+
+        override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+            val title = findViewById<EditText>(id.session_title).text.toString().trim()
+            val date = findViewById<EditText>(id.session_date).text.toString().trim()
+            val time = findViewById<EditText>(id.session_time).text.toString().trim()
+            val description = findViewById<EditText>(id.session_description).text.toString().trim()
+
+            findViewById<Button>(id.next).isEnabled = title.isNotEmpty() && date.isNotEmpty() && time.isNotEmpty() && description.isNotEmpty()
+        }
+
+        override fun afterTextChanged(s: Editable) {}
     }
 
     fun initDateTimePicker(){
@@ -152,25 +198,41 @@ class AddSessionToPitchActivity : AppCompatActivity() {
 
     fun initSaveBtn(){
 
-        //when black is pressed
+        /*//when back is pressed
         add_session_toolbar.setNavigationOnClickListener {
             onBackPressed()
-        }
+        }*/
 
         //when we click to save the new session
-        add_session_toolbar_save.isClickable = true
+        //add_session_toolbar_save.isClickable = true
         add_session_toolbar_save.paintFlags = add_session_toolbar_save.paintFlags or Paint.UNDERLINE_TEXT_FLAG
         add_session_toolbar_save.setOnClickListener {
-            val session = addSessionViewModel.getSession()
-            //TODO handle error in isSession valid (empty mandatory stuffs) )by checking
-            if(isSessionValid(session)) {
-                addSessionViewModel.onAddSessionClicked(session)
-                Toast.makeText(this, "Session added", Toast.LENGTH_LONG).show()
-                startActivity(AllSessionOfAPitchActivity.getIntent(this, addSessionViewModel.pitch))
-
+            if(added_players_recycle_view.size == 1){
+                AlertDialog.Builder(this)
+                    .setTitle("You did not add any player")
+                    .setMessage("Anyone will be able to join the session.")
+                    .setPositiveButton("Add Players", null)
+                    .setNegativeButton("Continue"){ _, _ -> CreateSession()}
+                    //.setIcon(android.R.drawable.ic_dialog_alert)
+                    .show()
+                    .getButton(AlertDialog.BUTTON_POSITIVE).setTextColor(Color.parseColor("#1CA6BE"))
             }
+            else
+                CreateSession()
         }
     }
+
+    fun CreateSession(){
+        val session = addSessionViewModel.getSession()
+        //TODO handle error in isSession valid (empty mandatory stuffs) )by checking
+        if(isSessionValid(session)) {
+            addSessionViewModel.onAddSessionClicked(session)
+            Toast.makeText(this, "Session added", Toast.LENGTH_LONG).show()
+            startActivity(AllSessionOfAPitchActivity.getIntent(this, addSessionViewModel.pitch))
+
+        }
+    }
+
     fun  isSessionValid(session: Session): Boolean{
         //TODO belami
         return true
@@ -189,6 +251,21 @@ class AddSessionToPitchActivity : AppCompatActivity() {
         //price per player
         UIUtils.addOnTextViewChange(session_price_player, addSessionViewModel.price_player_input)
 
+    }
+
+    override fun onBackPressed() {
+        if(findViewById<LinearLayout>(id.session_info).visibility == View.GONE)
+            findViewById<LinearLayout>(id.session_info).visibility = View.VISIBLE
+        else{
+            AlertDialog.Builder(this)
+                .setTitle("You are about to stop creating a session")
+                .setMessage("You will lose all progress you've made")
+                .setPositiveButton("Continue", null)
+                .setNegativeButton("Stop"){ _, _ -> super.onBackPressed() }
+                //.setIcon(android.R.drawable.ic_dialog_alert)
+                .show()
+                .getButton(AlertDialog.BUTTON_POSITIVE).setTextColor(Color.parseColor("#1CA6BE"))
+        }
     }
 
 }
